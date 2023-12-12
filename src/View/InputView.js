8e5dccb05d41d2c9d8a2ac52ee2b0e5145aca7c6
jueun-park98 <jooeun06161@gmail.com>
2023-12-12 23:04:40 +0900

    feat: 코치 이름 및 못 먹는 메뉴 입력 기능 추가

---
+++ src/View/InputView.js
@@ -0,0 +1,18 @@
+import { Console } from '@woowacourse/mission-utils';
+import { COACH_NAMES_INPUT_MESSAGE, DISLIKED_MENUS_INPUT_MESSAGE } from '../constants';
+
+const InputView = {
+  async readCoachNames() {
+    const nameInput = await Console.readLineAsync(COACH_NAMES_INPUT_MESSAGE);
+
+    return nameInput;
+  },
+
+  async readDislikedMenus(coachName) {
+    const dislikedMenuInput = await Console.readLineAsync(`${coachName}${DISLIKED_MENUS_INPUT_MESSAGE}`);
+
+    return dislikedMenuInput;
+  }
+}
+
+export default InputView;
--- src/constants.js
+++ src/constants.js
@@ -12,3 +12,6 @@ export const END_MESSAGE = '추천을 완료했습니다.';
 export const RESULT_MESSAGE = `메뉴 추천 결과입니다.
 [ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]
 [ 카테고리 | 한식 | 한식 | 일식 | 중식 | 아시안 ]`;
+
+export const COACH_NAMES_INPUT_MESSAGE = '메뉴 추천을 받을 코치의 이름을 입력 해 주세요.';
+export const DISLIKED_MENUS_INPUT_MESSAGE = '(이)가 못 먹는 메뉴를 입력해 주세요.';
